9dd620751c30f7c501a19bfdbe6e00a463dbc635
Dave Cheng <dave.cheng@ycdsbk12.ca>
2024-10-02 10:45:57 -0400

    Updated Smartphone example

--- src/myDemo/Main.java
+++ src/myDemo/Main.java
@@ -2,8 +2,12 @@ package myDemo;
 
 public class Main {
     public static void main(String[] args) {
+        // create new objects
         Smartphone davesPhone = new Smartphone("iPhone 12 mini", 64, 5.4, true);
+        Smartphone ericsPhone = new Smartphone("iPhone 16", 128, 6.1, true);
 
+        // display information
         System.out.println(davesPhone);
+        System.out.println(ericsPhone);
     }
 }
